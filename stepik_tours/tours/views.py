ee2dedcdbcf517a7f02de6aafaba2b94ce59ffa2
user <cagamor@list.ru>
2022-02-20 15:31:24 +0300

    add 404 and 500

--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden, HttpResponseServerError
 # Create your views here.
 
 def main_view(request):
@@ -13,3 +12,12 @@ def departure_view(request,departure):
 def tour_view(request, id ):
     context = {"id": id}
     return render(request, 'tour/tour.html', context=context)
+
+def custom_handler404(request, exception):
+    # Call when Http404 raised
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    # Call when raised some python exception
+    return HttpResponseServerError('Ошибка сервера!')
